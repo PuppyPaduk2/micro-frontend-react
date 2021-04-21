c410d10e6a0d794c7ddfbd1ff232c723d5d7d585
puppypaduk2 <debraooclark@gmail.com>
2021-04-21 10:11:13 +0300

    refactor: Remove old files

--- libs/hooks/__use-socket/index.ts
+++
@@ -1,6 +0,0 @@
-import { createSocket } from 'libs/socket';
-import { useMemo } from 'react';
-
-export const useSocket = (base: string, path: string) => {
-  return useMemo(() => createSocket(base, path), [base, path]);
-};
