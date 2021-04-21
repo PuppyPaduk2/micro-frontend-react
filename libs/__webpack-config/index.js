e99f41b56f2724ed9e8c9e7b189cde4ddca821bd
puppypaduk2 <debraooclark@gmail.com>
2021-04-21 19:23:14 +0300

    refactor: Remote old

--- libs/__webpack-config/index.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-const { baseHooks } = require("./base-hooks");
-const build = require("./build");
-
-module.exports = build(baseHooks);
